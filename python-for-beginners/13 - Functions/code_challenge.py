9b47cca1292af9f113f64b0238bbc73567c2ef94
SukhoiSu35 <160200996+SukhoiSu35@users.noreply.github.com>
2024-06-17 10:54:30 +1000

    Added module 4 calculator

--- python-for-beginners/13 - Functions/code_challenge.py
+++ python-for-beginners/13 - Functions/code_challenge.py
@@ -5,13 +5,31 @@
 # operation: the word 'add' or 'subtract'
 # the function should return the result of the two numbers added or subtracted
 # based on the value passed in for the operator
-#
+
+def calculator(first, second, operation):
+    if operation == '+': 
+        result = first + second
+        return result
+    elif operation == '-':
+        result = first - second
+        return result
+    elif operation == '/':
+        result = first / second
+        return result
+    else:
+        print("error values are invalid")
+    
+
 # Test your function with the values 6,4, add 
 # Should return 10
-#
+print(calculator(6, 4, '+'))
+
 # Test your function with the values 6,4, subtract 
 # Should return 2
-# 
+print(calculator(6,4,'-'))
+ 
 # BONUS: Test your function with the values 6, 4 and divide 
 # Have your function return an error message when invalid values are received
+
+print(calculator(6,4,''))
 
